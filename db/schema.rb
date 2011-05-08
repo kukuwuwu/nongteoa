4103dd7f8e9c227da1522ee5f2890c17254edd69
kukuwuwu <kukuwuwu@sina.comn>
2011-05-08 16:39:43 +0800

    reset countries migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,56 +10,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110418115220) do
-
-  create_table "cities", :force => true do |t|
-    t.string   "title"
-    t.integer  "province_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "content_people", :force => true do |t|
-    t.integer  "enterprise_id"
-    t.string   "name"
-    t.string   "position"
-    t.string   "phone_num"
-    t.string   "fax_num"
-    t.string   "mail"
-    t.string   "address"
-    t.string   "zip_code"
-    t.integer  "is_main"
-    t.string   "remark"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "enterprises", :force => true do |t|
-    t.integer  "admin_id"
-    t.string   "title"
-    t.integer  "province_id"
-    t.integer  "city_id"
-    t.integer  "industry_id"
-    t.string   "fax_num"
-    t.string   "phone_num"
-    t.string   "address"
-    t.string   "zip_code"
-    t.string   "mail"
-    t.string   "site"
-    t.string   "qq"
-    t.string   "remark"
-    t.string   "trademark"
-    t.text     "product"
-    t.text     "introduction"
-    t.integer  "status"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "provinces", :force => true do |t|
-    t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(:version => 0) do
 
 end
